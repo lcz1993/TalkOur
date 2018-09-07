ecad154285c7986a4014752a2b36ce967df835b7
lcz <lcz1993@163.com>
2018-09-07 18:32:12 +0800

    first commit

--- socket/pages/index/index.js
+++ socket/pages/index/index.js
@@ -4,50 +4,136 @@ const app = getApp()
 /**
  * @fileOverview 演示会话服务和 WebSocket 信道服务的使用方式
  */
-
-// 引入 QCloud 小程序增强 SDK
-var qcloud = require('../../vendor/qcloud-weapp-client-sdk/index');
-// 引入配置
-var config = require('../../config');
-// 显示繁忙提示
-var showBusy = text => wx.showToast({
-  title: text,
-  icon: 'loading',
-  duration: 10000
-});
-
-// 显示成功提示
-var showSuccess = text => wx.showToast({
-  title: text,
-  icon: 'success'
-});
-
-// 显示失败提示
-var showModel = (title, content) => {
-  wx.hideToast();
-
-  wx.showModal({
-    title,
-    content: JSON.stringify(content),
-    showCancel: false
-  });
-};
-
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    radioer: {
+      "id": "1",
+      "rad_name": "940不夜天",
+      "start_time": "00：00",
+      "end_time": "06：00",
+      "play_name": "姜飞",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/db51e16ed283507a796376b5ba3bd4b4.png",
+      "status": "2",
+      "relay": "0",
+      "saygood": "0"
+    },
+    radioList: [{
+      "id": "1",
+      "rad_name": "940不夜天",
+      "start_time": "00：00",
+      "end_time": "06：00",
+      "play_name": "姜飞",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/db51e16ed283507a796376b5ba3bd4b4.png",
+      "status": "2",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "2",
+      "rad_name": "音乐早上好",
+      "start_time": "06：00",
+      "end_time": "07：00",
+      "play_name": "王潇",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/321a4ab703a48e28b4380d5b957e2fc0.png",
+      "status": "1",
+      "relay": "10",
+      "saygood": "7"
+    }, {
+      "id": "3",
+      "rad_name": "柠檬咖啡tea",
+      "start_time": "07：00",
+      "end_time": "09：00",
+      "play_name": "亚东",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/cf6d99eb9da52c2f3f49a4482b7775bc.png",
+      "status": "0",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "4",
+      "rad_name": "音乐老友记",
+      "start_time": "09：00",
+      "end_time": "12：00",
+      "play_name": "石坚",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/efe5c98e2c0d18f12f3e6d03ec5c0892.png",
+      "status": "0",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "5",
+      "rad_name": "只听好歌不听话",
+      "start_time": "12：00",
+      "end_time": "14：00",
+      "play_name": "姜飞",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/4f34f919ff07d6e5297b096acd8e9701.png",
+      "status": "0",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "6",
+      "rad_name": "音乐红茶馆",
+      "start_time": "14：00",
+      "end_time": "17：00",
+      "play_name": "小栩",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/16a4fd7cdc86556992636bea0625fd69.png",
+      "status": "1",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "7",
+      "rad_name": "只听好歌不听话",
+      "start_time": "17：00",
+      "end_time": "19：00",
+      "play_name": "宏亮",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/7929fc2e82daa1c33e1e0199b91ca403.png",
+      "status": "0",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "8",
+      "rad_name": "爱上一座城",
+      "start_time": "19：00",
+      "end_time": "20：00",
+      "play_name": "王潇",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/6f8ccf02ae0ca877acc311e9ad8cf444.png",
+      "status": "1",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "9",
+      "rad_name": "只爱老情歌",
+      "start_time": "20：00",
+      "end_time": "22：00",
+      "play_name": "嘟嘟",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/760a1fce6dcb17d132ed19c08905ece0.png",
+      "status": "1",
+      "relay": "12",
+      "saygood": "9"
+    }, {
+      "id": "10",
+      "rad_name": "美文便当",
+      "start_time": "22：00",
+      "end_time": "23：00",
+      "play_name": "940DJ",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/2e25e74811562d2408f0087d6e4d045a.png",
+      "status": "0",
+      "relay": "0",
+      "saygood": "0"
+    }, {
+      "id": "11",
+      "rad_name": "边走边听",
+      "start_time": "23：00",
+      "end_time": "24：00",
+      "play_name": "王平",
+      "avatarurl": "https://986074128.tssccm.xyz/uploads/11d2ccd146dd223b8e02b4e7352f30bc.png",
+      "status": "1",
+      "relay": "10",
+      "saygood": "7"
+    }],
   },
   radiofun: function (e) {
-    console.log(e.currentTarget.dataset.id);
     wx.navigateTo({
       url: '../radio/radio?id=' + e.currentTarget.dataset.id
     })
@@ -67,30 +153,14 @@ Page({
     wx.getUserInfo({
       success: function (res) {
         that.setData({
-          'user': res.userInfo.avatarUrl,
+          'userImage': res.userInfo.avatarUrl,
         })
       }
     })
-    wx.request({
-      url: `https://${config.service.host}/radio/req`,
-      data: {},
-      method: 'POST',
-      header: {
-        'content-type': 'application/X-www-form-urlencoded;charset=utf-8'
-      },
-      success: (res) => {
-        console.log(res.data.data);
-        that.setData({
-          'radio': res.data.data,
-          'radioer': res.data.radioer[0]
-        })
-      }
-    });
   },
 
   onLoad: function () {
     var auth = new qcloud.authority();
-
   }
 
 })
